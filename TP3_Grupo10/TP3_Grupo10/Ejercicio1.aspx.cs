4b5c0864368af11fbb468af5052255dc0f14f7a8
Nahuel-1993 <nahuelfeijoo1993@gmail.com>
2025-04-19 00:07:33 -0300

    Custom validator para txtNombreLocalidad

--- TP3_Grupo10/TP3_Grupo10/Ejercicio1.aspx.cs
+++ TP3_Grupo10/TP3_Grupo10/Ejercicio1.aspx.cs
@@ -11,7 +11,36 @@ namespace TP3_Grupo10
     {
         protected void Page_Load(object sender, EventArgs e)
         {
+            Page.UnobtrusiveValidationMode = UnobtrusiveValidationMode.None;
+        }
 
+        protected void cvLocalidad_ServerValidate(object source, ServerValidateEventArgs args)
+        {
+            string nuevaLocalidad = args.Value.Trim(); // Lo que hay escrito en TextBox
+            bool existe = false;
+
+            // Recorreo los ítems del DropDownList
+            foreach (ListItem item in ddlLocalidades.Items)
+            {
+                if (item.Text.Equals(nuevaLocalidad, StringComparison.OrdinalIgnoreCase))
+                {
+                    existe = true;
+                    break;
+                }
+            }
+
+            // Si existe, no es válido (error)
+            args.IsValid = !existe;
+        }
+
+        protected void btnGuardarLocalidad_Click(object sender, EventArgs e)
+        {
+            if (Page.IsValid) // Solo si pasó el CustomValidator
+            {
+                string nuevaLocalidad = txtNombreLocalidad.Text.Trim();
+                ddlLocalidades.Items.Add(new ListItem(nuevaLocalidad));
+                txtNombreLocalidad.Text = ""; // Limpiar el textbox
+            }
         }
     }
 }
--- TP3_Grupo10/TP3_Grupo10/Ejercicio1.aspx.designer.cs
+++ TP3_Grupo10/TP3_Grupo10/Ejercicio1.aspx.designer.cs
@@ -33,6 +33,15 @@ namespace TP3_Grupo10
         protected global::System.Web.UI.WebControls.TextBox txtNombreLocalidad;
 
         /// <summary>
+        /// Control cvLocalidad.
+        /// </summary>
+        /// <remarks>
+        /// Campo generado automáticamente.
+        /// Para modificarlo, mueva la declaración del campo del archivo del diseñador al archivo de código subyacente.
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.CustomValidator cvLocalidad;
+
+        /// <summary>
         /// Control btnGuardarLocalidad.
         /// </summary>
         /// <remarks>
